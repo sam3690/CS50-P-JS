d5a344b5613b88356207f18fc5b4e2f91d571329
sam3690 <usamabinayoub@gmail.com>
2022-06-17 02:35:05 +0500

    Update 17-7-22, 2:34

---
+++ python/dictionaries.py
@@ -0,0 +1,6 @@
+houses = {"Harry": "Gryffyindor", "Draco": "Siyntherin"}
+
+houses ["Hermoine"] = "Gryffindor"
+
+name = input("Enter Student Name Here:  ")
+print(houses[name])
---
+++ python/loops.py
@@ -0,0 +1,9 @@
+# for i in range(7):
+#     print(i)
+
+
+# names = "{"Harry", "Ron", "Hermoine"}"
+name = "Harry"
+
+for character in name :
+    print(character)
